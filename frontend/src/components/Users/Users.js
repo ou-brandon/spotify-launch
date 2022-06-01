1a6b305d83d5233d0440c746a6e253e70c84e6e0
ou-brandon <73049480+ou-brandon@users.noreply.github.com>
2022-06-01 15:59:10 -0400

    Added table to discover page

--- frontend/src/components/Users/Users.js
+++ frontend/src/components/Users/Users.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import { Typography } from '@mui/material';
 import Navbar from '../Navbar/Navbar';
+import { TableContainer, Table, TableHead, TableBody, TableRow, TableCell, Paper} from '@mui/material';
 
 const Users = (props) => {
     const [users, setUsers] = useState([]);
@@ -15,10 +16,31 @@ const Users = (props) => {
     <>
         <Navbar/>
         <Typography variant='h4'>Discover Users</Typography>
-        <Typography variant='h6'>We will need to make a table to display this. For now, every public user's bio is displayed</Typography>
-        {users && users.map((user) => 
-            <h4>{user[1].isPublic && user[1].bio}</h4>
-        )}
+        <TableContainer component={Paper} sx={{}}>
+          <Table area-label='simple-table' stickyHeader>
+            <TableHead>
+              <TableRow>
+                <TableCell><Typography variant='h6'>Username</Typography></TableCell>
+                <TableCell><Typography variant='h6'>Bio</Typography></TableCell>
+                <TableCell><Typography variant='h6'>Top Artists</Typography></TableCell>
+                <TableCell align='center'><Typography variant='h6'>Top Songs</Typography></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {users && users.map((user) => (
+                <TableRow key={user[1].spotifyID} sx={{  
+                  '&:nth-of-type(odd)': {backgroundColor: 'lightskyblue'}, 
+                  '&:last-child td, &:last-child th': {border: 0}
+                  }}>
+                  <TableCell>{user[1].displayName}</TableCell>
+                  <TableCell>{user[1].bio}</TableCell>
+                  <TableCell>{user[1].topArtists}</TableCell>
+                  <TableCell align='center'>{user[1].topSongs}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
     </>
   );
 }
