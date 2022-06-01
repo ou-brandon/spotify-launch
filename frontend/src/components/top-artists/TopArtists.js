1e7a6e9c90aaf33fc6600e1694520a058fddac09
kellyzhangg <kellyzhangg12@gmail.com>
2022-06-01 14:38:39 -0500

    finished top artists

--- frontend/src/components/top-artists/TopArtists.js
+++ frontend/src/components/top-artists/TopArtists.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react'
+import Box from '@mui/material/Box'
 
 const TopArtists = () => {
     const CLIENT_ID = "3057a0e95eef4b47b5a882f47b2fa2da"
@@ -31,36 +32,38 @@ const TopArtists = () => {
         window.localStorage.removeItem("token")
     }
 
-    // GET
+    //GET
     const [topArtists, setTopArtists] = useState([]);
     const getTopArtists = async () => {
         const {data} = await axios.get(TOPARTISTS_ENDPOINT, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-        }
-    })}
+          headers: {Authorization: `Bearer ${token}`,}
+      })
+      setTopArtists(data.items);
+    }
   
     const Artist = (props) => {
       return (
         <>  
-          <p>{props.artist.name}</p>
+          <Box padding={3} margin={2} >{props.artist.name}</Box>
         </>
       )
     }
 
     return (
       <>
-        <h1>Top Artists</h1>
+        <h1>Spotify App</h1>
         {!token ?
           <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${TOPARTISTS_SCOPE}`}>
           Login to Spotify</a>
           : <button onClick={logout}>Logout</button>}
 
-          {token ?
-            <button onClick={getTopArtists}>Get Top Artists</button>
-          : <p>Sign in to your account above</p>
-          }
-          {topArtists && topArtists.map(artist => <Artist artist = {artist}/>)}   
+        {token ?
+          <button onClick={getTopArtists}>Get Top Artists</button>
+        : <p>Sign in to your account above</p>
+        }
+        <br></br>
+        <h3>Top Artists:</h3>
+        {topArtists && topArtists.map(artist => <Artist artist = {artist}/>)}   
       </>
     )
 }
