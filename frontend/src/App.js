815a0f9e9e782ee15fea9a8091fcbea607033927
kellyzhangg <kellyzhangg12@gmail.com>
2022-06-02 09:02:58 -0500

    fixing topartists

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,32 @@
-import react from "react";
+import logo from './logo.svg';
 import './App.css';
-import TopArtists from './components/top-artists/TopArtists'
+import Homepage from './components/Homepage/Homepage';
+import Users from './components/Users/Users';
+import Navbar from './components/Navbar/Navbar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { useState } from 'react';
+import { UserTokenContext } from './components/Context/UserTokenContext';
+import TopArtists from './components/top-artists/TopArtists';
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [accessToken, setAccessToken] = useState(null);
 
-    return (
-        <div className="App">
-            <TopArtists/>
-        </div>
-    );
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <UserTokenContext.Provider value={{user, setUser, accessToken, setAccessToken}}>
+          <Routes>
+                <Route path="/" element={<Navbar />}/>
+                <Route path="/home" element={<Homepage/>} />
+                <Route path="/discover" element={<Users />} />
+                <Route path="/topArtists" element={<TopArtists/>} />
+            </Routes>
+        </UserTokenContext.Provider>
+      </BrowserRouter>   
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/components/top-artists/TopArtists.js
+++ frontend/src/components/top-artists/TopArtists.js
@@ -1,36 +1,15 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react'
 import Box from '@mui/material/Box'
+import Navbar from '../Navbar/Navbar';
+import { useContext } from 'react';
+import { UserTokenContext } from '../Context/UserTokenContext';
 
 const TopArtists = () => {
-    const CLIENT_ID = "3057a0e95eef4b47b5a882f47b2fa2da"
-    const REDIRECT_URI = "http://localhost:3000"
-    const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
-    const RESPONSE_TYPE = "token"
     const TOPARTISTS_ENDPOINT = 'https://api.spotify.com/v1/me/top/artists'
-    const TOPARTISTS_SCOPE = 'user-top-read';
 
     const [token, setToken] = useState('')
-
-    useEffect(() => {
-        const hash = window.location.hash
-        let token = window.localStorage.getItem("token")
-
-        if (!token && hash) {
-            token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
-
-            window.location.hash = ""
-            window.localStorage.setItem("token", token)
-        }
-
-        setToken(token)
-
-    }, [])
-
-    const logout = () => {
-        setToken("")
-        window.localStorage.removeItem("token")
-    }
+    const {user, setUser, accessToken, setAccessToken} = useContext(UserTokenContext);
 
     //GET
     const [topArtists, setTopArtists] = useState([]);
@@ -43,29 +22,23 @@ const TopArtists = () => {
   
     const Artist = (props) => {
       return (
-        <>  
-          <Box padding={3} margin={2} >{props.artist.name}</Box>
-        </>
+      <>
+        <Box padding={3} margin={2} >{props.artist.name}</Box>
+      </>
       )
     }
 
     return (
       <>
-        <h1>Spotify App</h1>
-        {!token ?
-          <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${TOPARTISTS_SCOPE}`}>
-          Login to Spotify</a>
-          : <button onClick={logout}>Logout</button>}
-
-        {token ?
-          <button onClick={getTopArtists}>Get Top Artists</button>
-        : <p>Sign in to your account above</p>
-        }
+        <Navbar/>
+        <button onClick={getTopArtists}>Get Top Artists</button>
         <br></br>
         <h3>Top Artists:</h3>
         {topArtists && topArtists.map(artist => <Artist artist = {artist}/>)}   
       </>
     )
+
+    
 }
 
 export default TopArtists;
