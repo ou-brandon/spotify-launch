80b0d59af46d84a84fdf4fc18aa281aa3f1247c5
Emily Yuan <emilyyuan1220@gmail.com>
2022-06-03 09:48:29 -0400

    fix duplicate error

--- frontend/src/components/liked-songs/LikedSongs.js
+++ frontend/src/components/liked-songs/LikedSongs.js
@@ -1,9 +1,8 @@
-import {useState, useEffect} from "react";
+import {useState, useEffect, useContext} from "react";
 import axios from "axios"
 import { Box, Stack, Grid, GridItem } from '@mui/material'
 
 import Song from './Song.js'
-import { useContext, useEffect } from 'react';
 import { UserTokenContext } from '../Context/UserTokenContext';
 import Navbar from '../Navbar/Navbar';
 
