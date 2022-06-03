d8b25bbf88e5f9deb6c34c5f559f205c0ae37fdc
ou-brandon <73049480+ou-brandon@users.noreply.github.com>
2022-06-03 14:15:14 -0400

    Update TopArtists.js

--- frontend/src/components/top-artists/TopArtists.js
+++ frontend/src/components/top-artists/TopArtists.js
@@ -39,17 +39,10 @@ const TopArtists = () => {
         <Navbar/>
         {user ? 
           <div> 
-<<<<<<< HEAD
-            <Typography sx={{padding: '.5%'}} variant='h4'>Top Artists:</Typography>
+            <Typography sx={{padding: '.5%'}} variant='h4'>See Your Top Artists From:</Typography>
             <ToggleButtonGroup>            
               <ToggleButton onClick = {() => 
                 setTime('long_term')
-=======
-            <h3>See your Top Artists from: </h3>
-            <ToggleButtonGroup exclusive color = "primary" value = {activeButton} onChange = {handleButtonChange}>            
-              <ToggleButton value = "1" aria-label = "all time" onClick = {() => {
-                setTime('long_term')}
->>>>>>> 707429653a0988d27af3364192244b283d5b6809
               }>All Time</ToggleButton>
               <ToggleButton value = "2" aria-label = "past year" onClick = {() => {
                 setTime('medium_term')}
