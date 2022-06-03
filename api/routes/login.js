969353b8efc67a1a81b7d3058ec1359ac5ed8645
kellyzhangg <kellyzhangg12@gmail.com>
2022-06-03 10:41:20 -0500

    artists

--- api/routes/login.js
+++ api/routes/login.js
@@ -2,7 +2,6 @@ var express = require('express');
 var dotenv = require('dotenv').config();
 var router = express.Router();
 const request = require('request')
-const SCOPES = 'user-top-read';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,15 +10,13 @@ router.get('/', function(req, res, next) {
     const REDIRECT_URI = "http://localhost:3000/home";
     const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
     const RESPONSE_TYPE = "token";
-<<<<<<< HEAD
-=======
-    const SCOPES = "user-library-read"
+    const SCOPES = "user-top-read"
 
->>>>>>> 8b007f7f4ab4be044a07d3328b6e68dc90233269
     const logInReq = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPES}`;
     console.log('Response', logInReq);
     res.send(logInReq);
     console.log('Login link sent')
+    
     /*
     request(logInReq, {json:true}, (err, response, body) => {
         console.log(body);
